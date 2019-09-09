f16df9380c1c5f62c230d86ee1a664bec153e4c6
CPPtrueCoder <45642168+CPPtrueCoder@users.noreply.github.com>
2019-09-09 12:05:15 +0300

    Upload 9.09
    Add connection with signals and slots between two Forms

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,6 +4,15 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+connect(ui->choice_menu,&ChoiceExercises::clickedBtnMoving,ui->exercises,&MovingExercises::CheckMoving);
+connect(ui->choice_menu,&ChoiceExercises::clickedBtnStatic,ui->exercises,&MovingExercises::CheckStatic);
+connect(ui->choice_menu,&ChoiceExercises::clickedBtnGo,ui->exercises,&MovingExercises::ChoiceMoving);
+connect(ui->choice_menu,&ChoiceExercises::clickedBtnGo,ui->exercises,&MovingExercises::ChoiceStatic);
 }
+
+//MainWindow::MainWindow(QApplication *app):app();
+//{
+
+//}
 
 MainWindow::~MainWindow() { delete ui; }
--- mainwindow.h
+++ mainwindow.h
@@ -12,6 +12,7 @@ class MainWindow : public QMainWindow {
 
  public:
   explicit MainWindow(QWidget *parent = 0);
+   // explicit MainWindow(QApplication * app=nullptr);
   ~MainWindow();
 
  private:
